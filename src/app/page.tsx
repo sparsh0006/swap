78e68bdcd7a4bec30f6d25732453ee17730788a1
sparsh0006 <621sparsh@gmail.com>
2025-03-03 23:54:16 +0530

    updated UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,32 @@
+import Image from "next/image";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Link from "next/link";
+
 "use client";
-import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
-import { motion } from "framer-motion";
+import React from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Page() {
-  return (
-    <div className="bg-background text-foreground">
-
-      <div>sparsh</div>
-      
-
-      <HeroHighlight>
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-      >
-        With insomnia, nothing&apos;s real. Everything is far away. Everything
-        is a{" "}
-        <Highlight className="text-black dark:text-white">
-          copy, of a copy, of a copy.
-        </Highlight>
-      </motion.h1>
-    </HeroHighlight>
-  
-      
-    </div>
-  );
+    return (
+      <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-2xl mx-auto p-4">
+          <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+            SPARSH
+          </h1>
+          <p></p>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+            Building a instant swapping platform on jupiter that converts
+            your token into $USDC instantly for so that merchant get their
+            payment in stable coin.
+          </p>
+          <input
+            type="text"
+            placeholder="hi@sparshtwt.in"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          />
+        </div>
+        <BackgroundBeams />
+      </div>
+    );
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-const svgToDataUri = require("mini-svg-data-uri");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -22,19 +23,7 @@ const config: Config = {
     },
   },
   plugins: [
-    addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-dot-thick": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
+    addVariablesForColors
   ],
 };
 export default config;
