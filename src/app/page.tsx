f62eefa302d022d307233705d7e740373f3ebdfc
sparsh0006 <621sparsh@gmail.com>
2025-03-05 00:18:00 +0530

    added spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import Link from "next/link";
 "use client";
 import React from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { cn } from "@/lib/utils";
+import { Spotlight } from "@/components/ui/spotlight";
 
 export default function Page() {
     return (
       <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
             SPARSH
