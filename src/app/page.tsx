db64b0108d62522c294e46fd0c331672d361aef3
sparsh0006 <621sparsh@gmail.com>
2025-03-07 23:53:33 +0530

    added text effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import {
   IconBrandYoutube,
   IconWallet
 } from "@tabler/icons-react";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 // Dynamically import the wallet button with SSR disabled
 const SolanaWalletButton = dynamic(
@@ -82,6 +83,26 @@ const links = [
   },
 ];
 
+
+const words = [
+  {
+    text: "Build",
+  },
+  {
+    text: "awesome",
+  },
+  {
+    text: "apps",
+  },
+  {
+    text: "with",
+  },
+  {
+    text: "Aceternity.",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
+
 export default function Page() {
     return (
       <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
@@ -93,7 +114,9 @@ export default function Page() {
           <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
             SPARSH
           </h1>
-          <p></p>
+          
+          <TypewriterEffectSmooth words={words} />
+
           <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
             Building a instant swapping platform on jupiter that converts
             your token into $USDC instantly for so that merchant get their
